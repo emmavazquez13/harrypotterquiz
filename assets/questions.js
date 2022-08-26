0edc869dfa53be5341e6a42260ae661722d688b6
emma Vazquez <emmavazquez700@gmail.com>
2022-08-25 23:33:37 -0500

    added files

---
+++ assets/questions.js
@@ -0,0 +1,47 @@
+//creating an array and passing the number, questions, options, and answers 
+let questions = [
+    {
+        numb: 1, 
+        question: "What were Harry Potters parents names?",
+        answer: "James and Lily",
+        options: [
+            "Ryan and Mary",
+            "James and Lily",
+            "Patrick and Rose",
+            "Jerry and Lily"
+        ]
+    },
+    {
+        numb: 2, 
+        question: "What was the name of Harry Potters owl?",
+        answer: "Hedwig",
+        options: [
+            "Hedwig",
+            "Ruffles",
+            "Katie",
+            "Ron"
+        ]
+    },
+    {
+        numb: 3, 
+        question: "What position did Harry play in Quidditch?",
+        answer: "Seeker",
+        options: [
+            "Chaser",
+            "Beater",
+            "Seeker",
+            "Keeper"
+        ]
+    },
+    {
+        numb: 4, 
+        question: "What relation was Sirus Black to Harry?",
+        answer: "Godfather",
+        options: [
+            "Chaser",
+            "Beater",
+            "Seeker",
+            "Keeper"
+        ]
+    },
+];
